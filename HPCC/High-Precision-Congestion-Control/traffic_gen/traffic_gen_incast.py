c91aa09094b053e37c924479e8b33f5b55952cca
Andrea Migliorini <90915509+Kerby35@users.noreply.github.com>
2026-07-06 18:20:37 +0200

    trafficgen_incast

--- HPCC/High-Precision-Congestion-Control/traffic_gen/traffic_gen_incast.py
+++ HPCC/High-Precision-Congestion-Control/traffic_gen/traffic_gen_incast.py
@@ -5,123 +5,143 @@ import heapq
 from optparse import OptionParser
 from custom_rand import CustomRand
 
+
 class Flow:
-	def __init__(self, src, dst, size, t):
-		self.src, self.dst, self.size, self.t = src, dst, size, t
-	def __str__(self):
-		return "%d %d 3 100 %d %.9f"%(self.src, self.dst, self.size, self.t)
+    def __init__(self, src, dst, size, t):
+        self.src, self.dst, self.size, self.t = src, dst, size, t
+
+    def __str__(self):
+        return "%d %d 3 100 %d %.9f" % (self.src, self.dst, self.size, self.t)
+
 
 def translate_bandwidth(b):
-	if b == None:
-		return None
-	if type(b)!=str:
-		return None
-	if b[-1] == 'G':
-		return float(b[:-1])*1e9
-	if b[-1] == 'M':
-		return float(b[:-1])*1e6
-	if b[-1] == 'K':
-		return float(b[:-1])*1e3
-	return float(b)
+    if b == None:
+        return None
+    if type(b) != str:
+        return None
+    if b[-1] == 'G':
+        return float(b[:-1]) * 1e9
+    if b[-1] == 'M':
+        return float(b[:-1]) * 1e6
+    if b[-1] == 'K':
+        return float(b[:-1]) * 1e3
+    return float(b)
+
 
 def poisson(lam):
-	return -math.log(1-random.random())*lam
+    return -math.log(1 - random.random()) * lam
+
 
 if __name__ == "__main__":
-	port = 80
-	parser = OptionParser()
-	parser.add_option("-c", "--cdf", dest = "cdf_file", help = "the file of the traffic size cdf", default = "uniform_distribution.txt")
-	parser.add_option("-n", "--nhost", dest = "nhost", help = "number of hosts")
-	parser.add_option("-l", "--load", dest = "load", help = "the percentage of the traffic load to the network capacity, by default 0.3", default = "0.3")
-	parser.add_option("-b", "--bandwidth", dest = "bandwidth", help = "the bandwidth of host link (G/M/K), by default 10G", default = "10G")
-	parser.add_option("-t", "--time", dest = "time", help = "the total run time (s), by default 10", default = "10")
-	parser.add_option("-o", "--output", dest = "output", help = "the output file", default = "tmp_traffic.txt")
-	options,args = parser.parse_args()
+    port = 80
+    parser = OptionParser()
+    parser.add_option("-c", "--cdf", dest="cdf_file", help="the file of the traffic size cdf",
+                      default="uniform_distribution.txt")
+    parser.add_option("-n", "--nhost", dest="nhost", help="number of hosts")
+    parser.add_option("-l", "--load", dest="load",
+                      help="the percentage of the traffic load to the network capacity, by default 0.3", default="0.3")
+    parser.add_option("-b", "--bandwidth", dest="bandwidth", help="the bandwidth of host link (G/M/K), by default 10G",
+                      default="10G")
+    parser.add_option("-t", "--time", dest="time", help="the total run time (s), by default 10", default="10")
+    parser.add_option("-o", "--output", dest="output", help="the output file", default="tmp_traffic.txt")
+    options, args = parser.parse_args()
 
-	base_t = 2000000000
+    base_t = 2000000000
 
-	if not options.nhost:
-		print "please use -n to enter number of hosts"
-		sys.exit(0)
-	nhost = int(options.nhost)
-	load = float(options.load)
-	bandwidth = translate_bandwidth(options.bandwidth)
-	time = float(options.time)*1e9 # translates to ns
-	output = options.output
-	if bandwidth == None:
-		print "bandwidth format incorrect"
-		sys.exit(0)
+    if not options.nhost:
+        print
+        "please use -n to enter number of hosts"
+        sys.exit(0)
+    nhost = int(options.nhost)
+    load = float(options.load)
+    bandwidth = translate_bandwidth(options.bandwidth)
+    time = float(options.time) * 1e9  # translates to ns
+    output = options.output
+    if bandwidth == None:
+        print
+        "bandwidth format incorrect"
+        sys.exit(0)
 
-	fileName = options.cdf_file
-	file = open(fileName,"r")
-	lines = file.readlines()
-	# read the cdf, save in cdf as [[x_i, cdf_i] ...]
-	cdf = []
-	for line in lines:
-		x,y = map(float, line.strip().split(' '))
-		cdf.append([x,y])
+    fileName = options.cdf_file
+    file = open(fileName, "r")
+    lines = file.readlines()
+    # read the cdf, save in cdf as [[x_i, cdf_i] ...]
+    cdf = []
+    for line in lines:
+        x, y = map(float, line.strip().split(' '))
+        cdf.append([x, y])
 
-	# create a custom random generator, which takes a cdf, and generate number according to the cdf
-	customRand = CustomRand()
-	if not customRand.setCdf(cdf):
-		print "Error: Not valid cdf"
-		sys.exit(0)
+    # create a custom random generator, which takes a cdf, and generate number according to the cdf
+    customRand = CustomRand()
+    if not customRand.setCdf(cdf):
+        print
+        "Error: Not valid cdf"
+        sys.exit(0)
 
-	ofile = open(output, "w")
+    ofile = open(output, "w")
 
-	# generate flows
-	avg = customRand.getAvg()
-	avg_inter_arrival = 1/(bandwidth*load/8./avg)*1000000000
-	avg_inter_incast= 1/(bandwidth*nhost*0.02/8/(500000*60))*1000000000
-	print("interincast:%d"%(int(time /avg_inter_incast)))
-	
-	n_flow_estimate = int(time / avg_inter_arrival * nhost)
-	print("n_flow_interarrival %d"%n_flow_estimate)
-	n_flow_estimate = int(time / avg_inter_arrival * nhost)+ int(time /avg_inter_incast)
-	print("n_flow_estimate: %d"%n_flow_estimate)
-	n_flow = 0
-	print("poisons avg_inter_arrival: %f avg_inter_incast: %f sum: %f"%(int(poisson(avg_inter_arrival)), int(poisson(avg_inter_incast)),int(poisson(avg_inter_incast+avg_inter_arrival))))
-	print("avg_inter_arrival: %f avg_inter_incast: %f sum: %f"%(avg_inter_arrival, avg_inter_incast, avg_inter_incast+avg_inter_arrival))
-	ofile.write("%d \n\n"%n_flow_estimate)
-	host_list = [(base_t + int(poisson(avg_inter_arrival)), i) for i in range(nhost)]
-	heapq.heapify(host_list)
-	while len(host_list) > 0:
-		t,src = host_list[0]
-		inter_t_avg = int(poisson(avg_inter_arrival))
-		inter_t_incast = 0#int(poisson(avg_inter_incast))
-		if inter_t_avg > inter_t_incast :
-			inter_t = inter_t_avg
-			new_tuple = (src, t + inter_t)
-			dst = random.randint(0, nhost-1)
-			while (dst == src):
-				dst = random.randint(0, nhost-1)
-			if (t + inter_t > time + base_t):
-				heapq.heappop(host_list)
-			else:
-				size = int(customRand.rand())
-				if size <= 0:
-					size = 1
-				n_flow += 1;
-				ofile.write("%d %d 3 100 %d %.9f\n"%(src, dst, size, t * 1e-9))
-				heapq.heapreplace(host_list, (t + inter_t, src))	
-		else:
-			t,dst = host_list[0]
-			inter_t = inter_t_incast
-			if (t + inter_t > time + base_t):
-				heapq.heappop(host_list)
-			else:
-				for i in range(0, 60):
-					new_tuple = (dst, t + inter_t)
-					src = random.randint(0, nhost-1)
-					while (dst == src):
-						src = random.randint(0, nhost-1)
-					n_flow += 1;
-					ofile.write("%d %d 3 100 %d %.9f\n"%(src, dst, 500000, t * 1e-9))
-				heapq.heapreplace(host_list, (t + inter_t, dst))
+    # generate flows
+    avg = customRand.getAvg()
+    avg_inter_arrival = 1 / (bandwidth * load / 8. / avg) * 1000000000
+    avg_inter_incast = 1 / (bandwidth * nhost * 0.02 / 8 / (500000 * 60)) * 1000000000
+    print("interincast:%d" % (int(time / avg_inter_incast)))
 
-	ofile.seek(0)
-	ofile.write("%d"%n_flow)
-	ofile.close()
+    n_flow_estimate = int(time / avg_inter_arrival * nhost)
+    print("n_flow_interarrival %d" % n_flow_estimate)
+    n_flow_estimate = int(time / avg_inter_arrival * nhost) + int(time / avg_inter_incast)
+    print("n_flow_estimate: %d" % n_flow_estimate)
+    n_flow = 0
+    print("poisons avg_inter_arrival: %f avg_inter_incast: %f sum: %f" % (
+    int(poisson(avg_inter_arrival)), int(poisson(avg_inter_incast)),
+    int(poisson(avg_inter_incast + avg_inter_arrival))))
+    print("avg_inter_arrival: %f avg_inter_incast: %f sum: %f" % (
+    avg_inter_arrival, avg_inter_incast, avg_inter_incast + avg_inter_arrival))
+    ofile.write("%d \n\n" % n_flow_estimate)
+    host_list = [(base_t + int(poisson(avg_inter_arrival)), i) for i in range(nhost)]
+    print("host_list: %d" % (len(host_list)))
+    all_flows = []
+    heapq.heapify(host_list)
+    while len(host_list) > 0:
+        t, src = host_list[0]
+        inter_t = int(poisson(avg_inter_arrival))
+        new_tuple = (src, t + inter_t)
+        dst = random.randint(0, nhost - 1)
+        while (dst == src):
+            dst = random.randint(0, nhost - 1)
+        if (t + inter_t > time + base_t):
+            heapq.heappop(host_list)
+        else:
+            size = int(customRand.rand())
+            if size <= 0:
+                size = 1
+            n_flow += 1;
+            all_flows.append((t, src, dst, size))
+            heapq.heapreplace(host_list, (t + inter_t, src))
+    incast_list = [(base_t + int(poisson(avg_inter_incast)), random.randint(0, nhost - 1)) for i in
+                   range(int(time / avg_inter_incast) + 1)] #questo forza la a stargli dentro l'expcted incasts (nonfunziona bene e non era la versione originale)
+    heapq.heapify(incast_list)
+    while len(incast_list) > 0:
+        t, dst = incast_list[0]
+        inter_t = int(
+            poisson(avg_inter_incast))  # moltiplicare per 60 qua aiuta ma non capisco perch negli altri punti meglio
+        new_tuple = (dst, t + inter_t)
+        if (t + inter_t > time + base_t):
+            heapq.heappop(incast_list)
+        else:
+            for i in range(0, 60):
+                new_tuple = (dst, t + inter_t)
+                src = random.randint(0, nhost - 1)
+                while (dst == src):
+                    src = random.randint(0, nhost - 1)
+                n_flow += 1;
+                all_flows.append((t, src, dst, 500000))
+            heapq.heapreplace(incast_list, (t + inter_t, dst))
+    all_flows.sort(key=lambda x: x[0])
+    for f in all_flows:
+        ofile.write("%d %d 3 100 %d %.9f\n" % (f[1], f[2], f[3], f[0] * 1e-9))
+    ofile.seek(0)
+    ofile.write("flows:%d\n" % n_flow)
+    ofile.close()
 
 '''
 	f_list = []
